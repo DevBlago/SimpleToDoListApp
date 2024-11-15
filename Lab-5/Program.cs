4fe0756112bc0cb13b13b6b30a59df81d23b6a2b
Ivan Blagov <blagovivan112@gmail.com>
2024-11-15 11:50:43 +0300

    Initial commit: project setup

---
+++ Lab-5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomEnumerableExample
+{
+    public class RandomNumberSeries : IEnumerable<double>, IEnumerator<double>
+    {
+        private int _count;
+        private int _currentIndex = -1;
+        private double _currentValue;
+        private Random _random;
+
+        public RandomNumberSeries(int count)
+        {
+            _count = count;
+            _random = new Random();
+        }
+
+        // Реализация IEnumerator<double>
+        public double Current => _currentValue;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if (_currentIndex + 1 < _count)
+            {
+                _currentValue = _random.NextDouble();
+                _currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+
+        public void Dispose()
+        {
+            // Не требует ресурсов для освобождения, но метод необходим для интерфейса
+        }
+
+        // Реализация IEnumerable<double>
+        public IEnumerator<double> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int count = 10;
+            var randomSeries = new RandomNumberSeries(count);
+
+            Console.WriteLine("Сгенерированный ряд случайных чисел:");
+            foreach (var number in randomSeries)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
